0d3ad346e39a1429689ff525f3804d86da00fe46
Daniel Rojo <drojocent@uoc.edu>
2026-04-16 20:09:04 +0200

    create

---
+++ src/main/java/com/wodtracker/wod/WodtrackerWodServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wodtracker.wod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodtrackerWodServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WodtrackerWodServiceApplication.class, args);
+    }
+}
