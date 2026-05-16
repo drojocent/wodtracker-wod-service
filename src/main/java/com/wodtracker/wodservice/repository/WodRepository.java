0f8b01b256fba4fdc2be236979c3562d19351db4
Daniel Rojo <drojocent@uoc.edu>
2026-05-16 19:06:47 +0200

    fix date wods

--- src/main/java/com/wodtracker/wodservice/repository/WodRepository.java
+++ src/main/java/com/wodtracker/wodservice/repository/WodRepository.java
@@ -2,6 +2,7 @@ package com.wodtracker.wodservice.repository;
 
 import com.wodtracker.wodservice.entity.Wod;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,7 +12,12 @@ public interface WodRepository extends JpaRepository<Wod, Long> {
 
     Optional<Wod> findByDate(LocalDate date);
 
-    List<Wod> findByDateGreaterThanEqualOrderByDateAsc(LocalDate date);
+    @Query("""
+            SELECT w
+            FROM Wod w
+            ORDER BY CASE WHEN w.date IS NULL THEN 1 ELSE 0 END, w.date DESC
+            """)
+    List<Wod> findAllOrderByDateDescNullsLast();
 
     boolean existsByDate(LocalDate date);
 
